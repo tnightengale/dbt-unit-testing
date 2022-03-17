9179c652220559ea7721c0658e187f9464504b96
Teghan Nightengale <tnightengale@community.com>
2022-03-17 16:27:11 -0400

    temp: add debug()

--- macros/test_helpers.sql
+++ macros/test_helpers.sql
@@ -74,6 +74,7 @@
       {% endfor %}
       select {{ columns | join (",") }}
     {% else %}
+      {{ debug() }}
       {{ exceptions.raise_compiler_error("Source " ~ node.name ~ " columns must be declared in sources.yml, or it must exist in database") }}
     {% endif %}
   {% endif %}
